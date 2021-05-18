5fa3d08c9feb0aa14e174f8555a3a06a6e9433c8
fa94di <82581429+fa94di@users.noreply.github.com>
2021-05-18 13:38:07 +0200

    Delete jquery.js

--- jquery.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $("navbar-toggler").blur(function() {
-        var screenWidth = window.innerWidth;
-        if (screenWidth < 768) {
-            $("#navbarNav").collapse('hide');
-        }
-    });
-});
